a8d41f9b322bcfe380792ca7037a3b76c051c10a
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2021-10-25 07:48:14 -0400

    Ts file created and scripts { yeah. i come from node pls spare me!!! }

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { cheerio  } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
 
-
+// 377477
 async function loadData(link = ""){
+    try{
+        return cheerio.load(await (await fetch(link)).text());
+    }catch(e){
+        throw Error("If you ran this with --allow net, please check for proxy interference or if page is up!");
+    }
+}
 
-    return cheerio.load(await (await fetch(link)).text());
+async function getBookObject(book_code = ""){
+
+    let $ = await loadData(`https://nhentai.net/g/${ ( book_code | 0 )}/`);
 }
 
 
